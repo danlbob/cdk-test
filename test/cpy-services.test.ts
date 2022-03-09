4f9cc176c101080ce34fb27b9dea0c44a86aba5e
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-09 10:32:54 -0700

    Initial commit

---
+++ test/cpy-services.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CpyServices from '../lib/cpy-services-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cpy-services-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CpyServices.CpyServicesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
