02497be5a07f396b68f954e7e1106488050532eb
Tyler Bierwirth <tylerbierwirth@gmail.com>
2022-03-18 12:58:35 -0600

    Add secrets manager to core stack

--- lib/cpy-services-stack.ts
+++ lib/cpy-services-stack.ts
@@ -2,16 +2,19 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
-import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as sm from 'aws-cdk-lib/aws-secretsmanager';
 
 export class CpyServicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const apiKey = sm.Secret.fromSecretNameV2(this, 'ZenotiApiKey', `zenoti-${props?.stackName}-key`)
+    const zenotiUrl = sm.Secret.fromSecretNameV2(this, 'ZenotiBaseURL', `zenoti-${props?.stackName}-url`)
+
     const verifyGiftCardBalance = new lambda.Function(this, 'VerifyGiftCardHandler', {
       environment: {
-        apiKey: secretsmanager,
-        zenotiUrl: secretsmanager
+        apiKey: apiKey.toString(),
+        zenotiUrl: zenotiUrl.toString()
       },
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset('lambda'),
